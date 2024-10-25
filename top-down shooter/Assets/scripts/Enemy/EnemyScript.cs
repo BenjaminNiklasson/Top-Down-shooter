756a8aaede1572f93480b318a5d7beec8d2a1888
BenjaminNiklasson <benjamin.niklasson@elev.ga.lbs.se>
2024-10-25 13:45:55 +0200

    more fixes

--- top-down shooter/Assets/scripts/Enemy/EnemyScript.cs
+++ top-down shooter/Assets/scripts/Enemy/EnemyScript.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    [SerializeField] Transform player;
-    [SerializeField] float eSpeed = 2f;
-    Rigidbody2D rb; 
+    [SerializeField] float eSpeed = 3f;
+    Rigidbody2D rb;
+    GameObject player;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player") ;
     }
 
     void Update()
     {
         if (player != null)
         {
-            Vector2 direction = (player.position - transform.position).normalized;
+            Vector2 direction = (player.transform.position - transform.position).normalized;
             rb.MovePosition(rb.position + direction * eSpeed * Time.fixedDeltaTime);
         }
     }
--- top-down shooter/Assets/scripts/PlayerScripts/PlayerShooting.cs
+++ top-down shooter/Assets/scripts/PlayerScripts/PlayerShooting.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerShooting : MonoBehaviour
 {
     [SerializeField] float playerBulletSpeed = 4f;
     [SerializeField] GameObject playerBullet;
     [SerializeField] GameObject FunGun;
+    bool shotgunAquired = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,14 +20,36 @@ public class PlayerShooting : MonoBehaviour
 
     void OnFire()
     {
-        GameObject bullet = Instantiate(playerBullet, FunGun.transform.position, transform.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(transform.up * playerBulletSpeed, ForceMode2D.Impulse);
+        if (shotgunAquired == true)
+        {
+            for (int i = 30; i > -60; i -= 30)
+            {
+                float angle = 100 - i;
+                Quaternion rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+                GameObject bullet = Instantiate(playerBullet, FunGun.transform.position, rotation);
+                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                rb.AddForce(transform.up * playerBulletSpeed, ForceMode2D.Impulse);
+            }
+        }
+        else
+        {
+            GameObject bullet = Instantiate(playerBullet, FunGun.transform.position, transform.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(transform.up * playerBulletSpeed, ForceMode2D.Impulse);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Upgrade"))
+        {
+            shotgunAquired = true;
+        }
     }
 }
